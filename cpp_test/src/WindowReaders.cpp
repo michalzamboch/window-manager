64774d96594c45f74a2361c2e225873add065ba0
Michal Žamboch <zamboch.michal@seznam.cz>
2023-09-02 22:59:40 +0200

    Refactoring.

---
+++ cpp_test/src/WindowReaders.cpp
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "WinRect.h"
+
+BOOL CALLBACK GetWindowsTitles(HWND hwnd, LPARAM lParam){
+    const DWORD TITLE_SIZE = 1024;
+    WCHAR windowTitle[TITLE_SIZE];
+
+    GetWindowTextW(hwnd, windowTitle, TITLE_SIZE);
+
+    int length = ::GetWindowTextLength(hwnd);
+    std::wstring title(&windowTitle[0]);
+    if (!IsWindowVisible(hwnd) || length == 0 || title == L"Program Manager") {
+        return TRUE;
+    }
+    
+    std::vector<std::wstring>& titles = *reinterpret_cast<std::vector<std::wstring>*>(lParam);
+    titles.push_back(title);
+
+    return TRUE;
+}
+
+ BOOL CALLBACK GetWindowsRects(HWND hwnd, LPARAM lParam){
+    RECT rect;
+
+    GetWindowRect(hwnd, &rect);
+
+    if (!IsWindowVisible(hwnd)) {
+        return TRUE;
+    }
+    
+    WinRect winRect(rect);
+    std::vector<WinRect>& rectangles = *reinterpret_cast<std::vector<WinRect>*>(lParam);
+    rectangles.push_back(winRect);
+
+    return TRUE;
+}
---
+++ cpp_test/src/WindowReaders.h
@@ -0,0 +1,4 @@
+#include <windows.h>
+
+BOOL CALLBACK GetWindowsTitles(HWND hwnd, LPARAM lParam);
+BOOL CALLBACK GetWindowsRects(HWND hwnd, LPARAM lParam);
