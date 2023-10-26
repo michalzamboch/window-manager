19eaf7b6a42001e3c00e16ba7ad3f9414385a2eb
Michal Žamboch <zamboch.michal@seznam.cz>
2023-10-26 21:19:11 +0200

    New way of checking if the window is visible

--- manager-rs/src/connection/info.rs
+++ manager-rs/src/connection/info.rs
@@ -1,15 +1,13 @@
-use winapi::{
-    shared::windef::HWND,
-    um::winuser::IsWindowVisible,
-};
+use winapi::{shared::windef::HWND, um::winuser::IsWindowVisible};
 
-use super::title::get_title;
+use super::{size::get_window_placement, title::get_title};
 
 pub fn is_window_visible(window: &HWND) -> bool {
     let visible;
+    let placement = get_window_placement(window);
     unsafe {
         let title: String = get_title(*window);
-        visible = title.len() != 0 && IsWindowVisible(*window) != 0 && title != "Program Manager";
+        visible = title.len() != 0 && IsWindowVisible(*window) != 0 && placement.showCmd != 1;
     }
 
     visible
